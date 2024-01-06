70b0412fdacd019f89014ba2d23d55a6a06957df
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2024-01-06 04:35:10 -0300

    Finish (for now) the header component style. Add search box animation when focused. Make the logo a link to redirect to the home page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   faMagnifyingGlass = faMagnifyingGlass;
+  navbarVisible: boolean = true;
+  isSearchBoxExpanding: boolean = false;
 
   constructor(private router: Router) {}
+
+  @ViewChild('searchBox') searchBox!: ElementRef;
+
+  hideNavbar() {
+    this.navbarVisible = false;
+    this.isSearchBoxExpanding = true;
+  }
+
+  beginSearchBoxShrink() {
+    this.isSearchBoxExpanding = false;
+    this.navbarVisible = true;
+  }
+
+  handleTransitionEnd(event: TransitionEvent) {
+    if(event.propertyName === 'width' && !this.isSearchBoxExpanding) {
+      this.navbarVisible = true;
+    }
+  }
 
   onLoginClick() {
     this.router.navigate(['/login']);
@@ -22,4 +43,5 @@ export class HeaderComponent {
   onRegisterClick() {
     this.router.navigate(['/register']);
   }
+
 }
