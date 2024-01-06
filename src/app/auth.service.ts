42713b8f6238f8f6ef8c13086313bb9a6c15261b
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2024-01-06 06:39:30 -0300

    Make the login work and save the jwt token, style the login component

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,10 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   private registerUrl = 'https://localhost:7012/api/v1/Auth/register'
+  private loginUrl = 'https://localhost:7012/api/v1/Auth/login'
 
   constructor(private http: HttpClient) { }
 
   registerUser(user: any) {
     return this.http.post<any>(this.registerUrl, user);
+  }
+
+  login(user: any) {
+    return this.http.post<any>(this.loginUrl, user);
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -17,10 +18,18 @@ export class LoginComponent {
     password: ''
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
-  onLogin() {
-    console.log(this.user);
+  onLogin(form: NgForm) {
+      this.authService.login(form.value).subscribe(
+        res => {
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['/']);
+        },
+        err => {
+          console.error('Error', err)
+        }
+      )
   }
 
   onRegister() {
