2911e9ce56bbd9c49cb8402b57ea2aeb3cf7ab97
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2024-01-10 23:04:44 -0300

    change regex constant type do RegExp and add password regex

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,2 +1,3 @@
-export const emailRegex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-export const usernameRegex = "^[a-zA-Z0-9_-]{3,}$";
+export const emailRegex: RegExp = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const usernameRegex: RegExp = /^[a-zA-Z0-9_-]{3,}$/;
+export const strongPasswordRegex: RegExp = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/;
