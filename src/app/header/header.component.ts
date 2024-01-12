497a8b19211febbe72cd09d02b66c415e8fd4453
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2024-01-12 06:03:39 -0300

    Change header buttons when the user log in, add logout button and pfp picture and dropdown menu with some users options

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -15,10 +15,32 @@ export class HeaderComponent {
   faMagnifyingGlass = faMagnifyingGlass;
   navbarVisible: boolean = true;
   isSearchBoxExpanding: boolean = false;
+  dropdownVisible: boolean = false;
+  userProfileImageUrl = "https://play-lh.googleusercontent.com/KQyO1eE9c1nGD5EnHMEtA6pafHgnl6Vhgcarr3689fST8eYsU-f11GEDevwieynh5A=w240-h480-rw";
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private eRef: ElementRef) {}
 
   @ViewChild('searchBox') searchBox!: ElementRef;
+  @ViewChild('dropdownMenu', { static: false }) dropdownMenu!: ElementRef;
+  @ViewChild('dropdownToggle', { static: false })
+  dropdownToggle!: ElementRef;
+
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: { target: any; }) {
+    if (this.dropdownVisible && !this.dropdownMenu.nativeElement.contains(event.target) &&
+        !this.dropdownToggle.nativeElement.contains(event.target)) {
+      this.closeDropdown();
+    }
+  }
+
+  closeDropdown() {
+    this.dropdownVisible = false;
+    console.log('clicked outside');
+  }
 
   hideNavbar() {
     this.navbarVisible = false;
@@ -42,6 +64,18 @@ export class HeaderComponent {
 
   onRegisterClick() {
     this.router.navigate(['/register']);
+  }
+
+  isUserAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+
+    return !!token;
+  }
+
+  onLogoutClick() {
+    localStorage.removeItem('token');
+
+    this.router.navigate(['/login']);
   }
 
 }
