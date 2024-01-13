f3410087b3624a788d9a9cdd287edd78b85a392b
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2024-01-13 08:36:44 -0300

    create anime profile component

---
+++ src/app/anime-profile/anime-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anime-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './anime-profile.component.html',
+  styleUrl: './anime-profile.component.scss'
+})
+export class AnimeProfileComponent {
+
+}
