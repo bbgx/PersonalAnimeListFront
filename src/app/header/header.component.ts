fca621a720ef1a4ad6f900968ad45b2b86b4033d
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2024-01-12 22:22:05 -0300

    Add searchbox dropdown with the first 5 results, format and style it

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
+import { Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -12,13 +14,16 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private searchTerms = new Subject<string>();
+  searchResults: any[] = [];
   faMagnifyingGlass = faMagnifyingGlass;
   navbarVisible: boolean = true;
   isSearchBoxExpanding: boolean = false;
   dropdownVisible: boolean = false;
   userProfileImageUrl = "https://play-lh.googleusercontent.com/KQyO1eE9c1nGD5EnHMEtA6pafHgnl6Vhgcarr3689fST8eYsU-f11GEDevwieynh5A=w240-h480-rw";
+  showSearchResultsDropdown!: boolean;
 
-  constructor(private router: Router, private eRef: ElementRef) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   @ViewChild('searchBox') searchBox!: ElementRef;
   @ViewChild('dropdownMenu', { static: false }) dropdownMenu!: ElementRef;
@@ -42,6 +47,38 @@ export class HeaderComponent {
     console.log('clicked outside');
   }
 
+  searchAnime(term: string): void {
+    if (term) {
+      this.showSearchResultsDropdown = true;
+      this.searchTerms.next(term);
+    } else {
+      this.showSearchResultsDropdown = false;
+    }
+  }
+
+  hideSearchResults() {
+    this.showSearchResultsDropdown = false;
+  }
+
+  showSearchResults(value: string) {
+    if (value) {
+      this.searchAnime(value);
+    }
+  }
+
+  ngOnInit(): void {
+    this.searchTerms.pipe(
+      debounceTime(300),
+
+      distinctUntilChanged(),
+
+      switchMap((term: string) => this.http.get(`https://localhost:7012/api/v1/Anime?Q=${term}&limit=5`)),
+    ).subscribe(results => {
+      this.searchResults = (results as any).data;
+      console.log(results);
+    })
+  }
+
   hideNavbar() {
     this.navbarVisible = false;
     this.isSearchBoxExpanding = true;
@@ -52,10 +89,21 @@ export class HeaderComponent {
     this.navbarVisible = true;
   }
 
+  expandSearchBox() {
+    this.isSearchBoxExpanding = true;
+  }
+
   handleTransitionEnd(event: TransitionEvent) {
     if(event.propertyName === 'width' && !this.isSearchBoxExpanding) {
       this.navbarVisible = true;
     }
+  }
+
+  handleSearchBoxTransitionEnd() {
+    if (this.isSearchBoxExpanding && this.searchBox.nativeElement.value && this.searchResults.length > 0) {
+      this.showSearchResultsDropdown = true;
+    }
+    this.isSearchBoxExpanding = false;
   }
 
   onLoginClick() {
