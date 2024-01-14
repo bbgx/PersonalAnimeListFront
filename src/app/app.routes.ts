c7033ace36c67dc17978eb54a439bf6dbf4a1ea8
Bernardo Bugmann <bernardo_bugmann@protonmail.com>
2024-01-14 11:07:11 -0300

    Start anime profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AnimeProfileComponent } from './anime-profile/anime-profile.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'anime', component: AnimeProfileComponent },
 ];
